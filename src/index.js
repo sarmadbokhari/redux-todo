6cb2d5a0356a9539853e4e892fd6f76b4bf5e086
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-22 15:49:58 -0700

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Set up your application entry point here...
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router routes={routes} history={browserHistory} />,
+  document.getElementById('app')
+)
