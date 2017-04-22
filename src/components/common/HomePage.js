77c9eb826ae6e0173b0d93cf9190be0882740418
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-04-22 15:23:46 -0700

    Add Home component

---
+++ src/components/common/HomePage.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Redux Todo App</h1>
+      <p>Learning to bootstrap a React & Redux app</p>
+    </div>
+  )
+}
+
+export default Home;
